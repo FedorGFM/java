a956235bdb95ef2afa050ebade88e148ebd86ecf
Fedor <4ips228@gmail.com>
2023-04-27 16:48:21 +0700

    Lesson 3

--- src/main/java/lesson3/Task2_2.java
+++ src/main/java/lesson3/Task2_2.java
@@ -1,14 +1,32 @@
 package lesson3;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Task2_2 {
     public static void main(String[] args) {
 //        Задание №2.2 Пройти по списку из предыдущего задания и удалить повторяющиеся элементы.
 
+        String[] planetsNamesStringArray = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Neptune", "Uranus", "Pluto"};
+        Random random = new Random();
+        List<String> randomSunSystem = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            randomSunSystem.add(planetsNamesStringArray[random.nextInt(planetsNamesStringArray.length)]);
+        }
 
+        randomSunSystem.sort(Comparator.naturalOrder());
+        System.out.println(randomSunSystem);
+
+
+        for (int i = 0; i < randomSunSystem.size(); i++) {
+            Set<String> setSSList = new LinkedHashSet<>(randomSunSystem);
+            randomSunSystem.clear();
+            randomSunSystem.addAll(setSSList);
+        }
+        System.out.println(randomSunSystem);
+
+        // 2nd Method with distinct() application:
+        //List<String> listWithoutDuplicates =
+        //        randomSunSystem.stream().distinct().collect(Collectors.toList());
+        //System.out.println(randomSunSystem);
     }
 }
